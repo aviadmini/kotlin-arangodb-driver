220876139d153bc55010f48faf6340e2783311fe
aviadmini <avirockets@gmail.com>
2022-07-20 15:15:19 +0200

    change creators receiver

--- src/main/kotlin/pw/avi/arangodbdriver/creators.kt
+++ src/main/kotlin/pw/avi/arangodbdriver/creators.kt
@@ -5,7 +5,7 @@ package pw.avi.arangodbdriver
 import com.arangodb.ArangoDB
 import com.arangodb.async.ArangoDBAsync
 
-fun arangoDb(builder: (ArangoDB.Builder) -> Unit): ArangoDB = ArangoDB.Builder().apply(builder).build()
+fun arangoDb(builder: ArangoDB.Builder.() -> Unit): ArangoDB = ArangoDB.Builder().apply(builder).build()
 
-fun arangoDbAsync(builder: (ArangoDBAsync.Builder) -> Unit): ArangoDBAsync = ArangoDBAsync.Builder().apply(builder).build()
+fun arangoDbAsync(builder: ArangoDBAsync.Builder.() -> Unit): ArangoDBAsync = ArangoDBAsync.Builder().apply(builder).build()
 
